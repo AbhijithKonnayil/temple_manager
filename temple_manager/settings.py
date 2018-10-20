f83d72fc733464f48222f8a36f72b25465cd8eb7
Abhijith K <iamabhijith.k@gmail.com>
2018-10-21 00:48:23 +0530

    configure postgresql database

--- temple_manager/settings.py
+++ temple_manager/settings.py
@@ -74,11 +74,17 @@ WSGI_APPLICATION = 'temple_manager.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'temple_db',
+        'USER': 'temple_admin',
+        'PASSWORD': 'temple_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        }
     }
-}
+
+
 
 
 # Password validation
