2da97ff7a6e4405eb6331d37df0b9af2b96d9a92
Abhijith K <iamabhijith.k@gmail.com>
2018-10-21 02:14:51 +0530

    implemet reciept - receipt item relation ship

--- receipt_management/models.py
+++ receipt_management/models.py
@@ -9,22 +9,29 @@ class Vazhipadu(models.Model):
     def __str__(self):
         return "{}".format(self.title)
 
-
-class ReceiptItem(models.Model):
-    vazhipadu = models.ForeignKey(Vazhipadu,null=True,on_delete=models.SET_NULL)
-    count = models.IntegerField()
-    amount = models.FloatField()
-
-    def __str__(self):
-        return "{} {}".format(self.id,self.vazhipadu)
-
 class Receipt(models.Model):
-    STAR_CHOICES=(('a','2'),('b','1'))
+    STAR_CHOICES=(('അശ്വതി','അശ്വതി'),('ഭരണി','ഭരണി'),('കാർത്തിക','കാർത്തിക'),
+                    ('രോഹിണി','രോഹിണി'),('മകയിരം','മകയിരം'),('തിരുവാതിര','തിരുവാതിര'),
+                    ('പുണർതം','പുണർതം'),('പൂയം','പൂയം'),('ആയില്യം','ആയില്യം'),
+                    ('മകം','മകം'),('പൂരം','പൂരം'),('ഉത്രം','ഉത്രം'),
+                    ('അത്തം','അത്തം'),('ചിത്തിര ','ചിത്തിര '),('ചോതി','ചോതി'),
+                    ('വിശാഖം','വിശാഖം'),('അനിഴം','അനിഴം'),('തൃക്കേട്ട','തൃക്കേട്ട'),
+                    ('  മൂലം',' മൂലം'),('പൂരാടം','പൂരാടം'),('ഉത്രാടം','ഉത്രാടം'),
+                    ('തിരുവോണം','തിരുവോണം'),('അവിട്ടം','അവിട്ടം'),('ചതയം','ചതയം'),    
+                    ('പൂരുരുട്ടാതി','പൂരുരുട്ടാതി'),('ഉത്രട്ടാതി','ഉത്രട്ടാതി'),('രേവതി','രേവതി'),
+                )
     name = models.CharField(max_length=50,null=False,blank=False)
-    star = models.CharField(max_length=20, choices=STAR_CHOICES, default='1')
-    receipt_items= models.ManyToManyField(ReceiptItem)
+    star = models.CharField(max_length=20, choices=STAR_CHOICES,)
     total_amount = models.FloatField()
 
     def __str__(self):
         return "{} {} {}".format(self.id,self.name,self.star)
-        
+
+class ReceiptItem(models.Model):
+    vazhipadu = models.ForeignKey(Vazhipadu,null=True,on_delete=models.SET_NULL)
+    count = models.IntegerField()
+    receipt= models.ForeignKey(Receipt,related_name='receipt_item',on_delete=models.CASCADE,null=False,blank=True)
+    amount = models.FloatField()
+
+    def __str__(self):
+        return "{} {}".format(self.id,self.vazhipadu)
