a27dbaab4f3b39c6d4e581a58f6b4173f9574bf7
Abhijith K <iamabhijith.k@gmail.com>
2019-04-23 18:34:22 +0530

    corrected double click issue

--- temple_manager/settings.py
+++ temple_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+nj*9888g76j@c=w55(r4@kruvb4a)hc+0fvx8_$ahz!djh^ql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
